e64f1cdc84a86c2dbbd94e6ad21909b83d674cd8
manavjp <manavjp.business@gmail.com>
2026-05-31 20:10:29 -0700

    v0.1 portfolio launch
    Initial portfolio website release with unfinished pages

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+/**
+ * Site-wide constants.
+ * Edit once here — changes propagate everywhere these are imported.
+ */
+
+export const RESUME_URL =
+  'https://drive.google.com/file/d/103-kHaMhd_KKaYjAb0eAIIGP14qsoP1J/view?usp=sharing';
